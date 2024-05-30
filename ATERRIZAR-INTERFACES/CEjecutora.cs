69914318db471aaa9326110f6d2e013baf89a96c
MatiasOrtigoza <91295462+MatiasOrtigoza@users.noreply.github.com>
2024-05-29 23:06:07 -0300

    Add files via upload

---
+++ ATERRIZAR-INTERFACES/CEjecutora.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATERRIZAR_INTERFACES
+{
+    internal class CEjecutora
+    {
+        static int TamañoVector = 2;
+
+        static void Main(string[] args)
+        {
+            CViaje[] VectorViajes = new CViaje[TamañoVector];
+            int Opcion = 0;
+
+            Console.Write($"\nCANTIDAD DE VIAJES A ANOTAR: {TamañoVector}\n");
+
+            CargarVector(VectorViajes);
+
+            Opcion = MostrarMenu();
+            if (Opcion == 1)
+            {
+                BuscarViajePorCodigo(VectorViajes);
+            }
+            else if(Opcion == 2)
+            {
+                Console.Write("\nIngrese el origen para ver los viajes que lo comparten: ");
+                VuelosOrigenCompartido(VectorViajes, Console.ReadLine());
+            }
+            else if (Opcion == 3)
+            {
+                Console.Write("\nIngrese el destino para ver los viajes que lo comparten: ");
+                VuelosDestinoCompartido(VectorViajes, Console.ReadLine());
+            }
+            
+
+            Console.Write("\nLISTA DE VIAJES (SIN ORDENAR)" + MostrarViajes(VectorViajes));
+            Array.Sort(VectorViajes);
+            Console.Write("\n\nLISTA DE VIAJES (ORDENADOS POR PRECIO)" + MostrarViajes(VectorViajes));
+            Array.Reverse(VectorViajes);
+            Console.Write("\n\nLISTA DE VIAJES (REVERSED)" + MostrarViajes(VectorViajes));
+            Console.ReadKey();
+
+
+
+            Console.Write("\n\nSaliendo...Presione una tecla para continuar.");
+            Console.ReadKey();
+
+        }
+
+        static string MostrarViajes(CViaje[] Vector)
+        {
+            string Datos = "";
+
+            foreach(CViaje Viaje in Vector)
+            {
+                Datos = Datos + Viaje.DarDatos();
+            }
+
+            return Datos;
+        }
+
+        static void VuelosDestinoCompartido(CViaje[] VectorViajes, string Destino)
+        {
+            int i;
+            int CantidadEncontrada = 0;
+
+            for (i = 0; i < VectorViajes.Length; i++)
+            {
+                if (VectorViajes[i].GetDestino() == Destino)
+                {
+                    CantidadEncontrada++;
+                    Console.Write(VectorViajes[i].DarDatos());
+                }
+            }
+
+            CasosCompartidos(Destino, CantidadEncontrada);
+        }
+
+        static void VuelosOrigenCompartido(CViaje[] VectorViajes, string Origen)
+        {
+            int i;
+            int CantidadEncontrada = 0;
+
+            for (i = 0; i < VectorViajes.Length; i++)
+            {
+                if (VectorViajes[i].GetOrigen() == Origen)
+                {
+                    CantidadEncontrada++;
+                    Console.Write(VectorViajes[i].DarDatos());
+                }
+            }
+
+            CasosCompartidos(Origen, CantidadEncontrada);
+        }
+
+        static void CasosCompartidos(string Dato, int CantidadEncontrada)
+        {
+            switch (CantidadEncontrada)
+            {
+                case 0:
+                    Console.Write("\nNo existen viajes que hayan tenido ese origen/destino...");
+                    Console.ReadKey();
+                    break;
+                case 1:
+                    Console.Write("\nHay un solo viaje que haya tenido ese origen/destino, el cual no lo comparte con nadie...");
+                    Console.ReadKey();
+                    break;
+                default:
+                    Console.Write($"\nHay {CantidadEncontrada} viajes que comparten el origen/destino {Dato}...");
+                    Console.ReadKey();
+                    break;
+            }
+        }
+
+        static void BuscarViajePorCodigo(CViaje[] VectorViajes)
+        {
+            string Codigo;
+            int i;
+            int Cuotas;
+
+            Console.Write("\nIngresa el código del viaje: ");
+            Codigo = Console.ReadLine();
+
+            for (i = 0; i < VectorViajes.Length; i++)
+            {
+                if (Codigo == VectorViajes[i].GetCodigo())
+                {
+                    Console.Write(VectorViajes[i].DarDatos());
+
+                    Console.Write("Ingrese la cantidad de cuotas a pagar el viaje (1-3-6-12): ");
+                    Cuotas = SolicitarCuotas();
+                    Console.Write($"\nEl total a pagar en {Cuotas} cuotas es: ${VectorViajes[i].DarPrecio(Cuotas)}...");
+                    Console.ReadKey();
+
+                    i = VectorViajes.Length + 1;
+                }
+            }
+
+            if (i != VectorViajes.Length + 1)
+            {
+                Console.Write("\n\nNo existe el viaje que está buscando...");
+                Console.ReadKey();
+            }
+
+        }
+        static void CargarVector(CViaje[] VectorViajes)
+        {
+            int Cont = 0;
+            string Origen;
+            string Destino;
+            string Codigo;
+            float Precio;
+
+            while (Cont < VectorViajes.Length)
+            {
+                Console.Write("\nIngresa el código del viaje: ");
+                Codigo = Console.ReadLine();
+                Console.Write("Ingresa el origen del viaje: ");
+                Origen = Console.ReadLine();
+                Console.Write("Ingresa el destino del viaje: ");
+                Destino = Console.ReadLine();
+                Console.Write("Ingresa el precio del viaje: ");
+                Precio = SolicitarPrecio();
+
+                VectorViajes[Cont] = new CViaje(Codigo, Origen, Destino);
+                VectorViajes[Cont].PrecioViaje = Precio;
+                Cont++;
+            }
+            
+        }
+
+        static int MostrarMenu()
+        {
+            int Opcion = 0;
+            Console.Write("\n\n\t\tMENU:\n\n1.Buscar un viaje por código\n2.Ingrese un origen para ver los vuelos que lo comparten.\n3. Ingrese un destino para ver los vuelos que lo comparten\n4.Salir\nIngrese: ");
+            return Opcion = SolicitarOpcion();
+        }
+
+
+        static int SolicitarOpcion()
+        {
+            int Opcion = 0;
+            bool Resultado;
+
+            Resultado = int.TryParse(Console.ReadLine(), out Opcion);
+            while (Resultado == false || OpcionInvalida(Opcion))
+            {
+                Console.Write($"\n{Opcion} no es una opción válida...\nIngrese nuevamente (1-4): ");
+                Resultado = int.TryParse(Console.ReadLine(), out Opcion);
+            }
+            return Opcion;
+        }
+
+
+        static float SolicitarPrecio()
+        {
+            float Precio = 0;
+            bool Resultado;
+
+            Resultado = float.TryParse(Console.ReadLine(), out Precio);
+            while (Resultado == false || Precio < 0)
+            {
+                Console.Write("El dato ingresado no es válido...\nIngrese nuevamente: ");
+                Resultado = float.TryParse(Console.ReadLine(), out Precio);
+            }
+
+            return Precio;
+        }
+
+        static int SolicitarCuotas()
+        {
+            int Cuotas = 0;
+            bool Resultado;
+
+            Resultado = int.TryParse(Console.ReadLine() , out Cuotas);
+            while (!Resultado || CuotaInvalida(Cuotas))
+            {
+                Console.Write("\nLa cuota ingresada no es válida...\nIngrese nuevamente: ");
+                Resultado = int.TryParse(Console.ReadLine(), out Cuotas);
+            }
+
+            return Cuotas;
+        }
+
+        static bool CuotaInvalida(int Cuotas)
+        {
+            if (Cuotas != 1 && Cuotas != 3 && Cuotas != 6 && Cuotas != 12)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        static bool OpcionInvalida(int Opcion)
+        {
+            if (Opcion < 1 || Opcion > 4)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ ATERRIZAR-INTERFACES/CViaje.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATERRIZAR_INTERFACES
+{
+    internal class CViaje:IComparable
+    {
+        string Codigo;
+        string Origen;
+        string Destino;
+        float Precio;
+
+        public float PrecioViaje { get => Precio; set => Precio = value; }
+
+        public CViaje(string Codigo, string Origen, string Destino)
+        {
+            this.Codigo = Codigo;
+            this.Origen = Origen;
+            this.Destino = Destino;
+        }
+
+        public CViaje()
+        {
+            this.Codigo = "undefined";
+            this.Origen = "undefined";
+            this.Destino = "undefined";
+            this.Precio = 0F;
+        }
+
+        public string GetOrigen()
+        {
+            return this.Origen;
+        }
+
+        public string GetDestino()
+        {
+            return this.Destino;
+        }
+
+        public string GetCodigo()
+        {
+            return this.Codigo;
+        }
+
+        public float DarPrecio(int Cuotas)
+        {
+            switch (Cuotas)
+            {
+                case 1:
+                    return this.Precio;
+                case 3:
+                    return this.Precio * 1.1F;
+                case 6:
+                    return this.Precio * 1.2F;
+                case 12:
+                    return this.Precio * 1.4F;
+                default:
+                    return -1;
+            }
+        }
+
+        public string DarDatos()
+        {
+            return $"\nCodigo: {this.Codigo}\nOrigen: {this.Origen}\nDestino: {this.Destino}\nPrecio base: {PrecioViaje}\n\n";
+        }
+        
+        public int CompareTo(object obj)
+        {
+            if (obj is CViaje)
+            {
+                CViaje Viaje2 = (CViaje)obj;
+
+                if (this.PrecioViaje < Viaje2.PrecioViaje)
+                {
+                    return -1;
+                }
+                if (this.PrecioViaje > Viaje2.PrecioViaje)
+                {
+                    return 1;
+                }
+
+                return 0;
+            }
+
+            return int.MaxValue;
+        }
+        
+    }
+}
